539ff4463faadf314206cd546c7f0fcd4071dc7a
Bernards Gulbis <bernards.gulbis@visma.com>
2026-02-22 20:15:54 +0200

    default schema and language

--- web/src/i18n/index.ts
+++ web/src/i18n/index.ts
@@ -14,6 +14,7 @@ i18n
     },
     fallbackLng: 'en',
     supportedLngs: ['en', 'lv'],
+    load: 'languageOnly',
     detection: {
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
