255d8b382b2dacdac19aaf2f3fa013ed856171b0
Bernards Gulbis <bernards.gulbis@visma.com>
2026-02-25 12:51:06 +0200

    sonar qube

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -15,5 +15,9 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
   },
 })
