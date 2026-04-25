ead2e1b4edc8dcb2559655eef63f62a4b4b255cc
Bernards Gulbis <bernards.gulbis@visma.com>
2026-04-25 22:27:45 +0300

    fix: ensure typeFilter is sorted correctly using localeCompare in TransactionView

--- web/src/components/TransactionView.tsx
+++ web/src/components/TransactionView.tsx
@@ -42,7 +42,7 @@ export const TransactionView = () => {
   }
 
   const debouncedTicker = useDebounce(tickerSearch, 150);
-  const typeFilterKey = [...typeFilter].sort().join(',');
+  const typeFilterKey = [...typeFilter].sort((a, b) => a.localeCompare(b)).join(',');
 
   // Track previous filter key as state to reset page when filters change
   const filterToken = `${debouncedTicker}|${typeFilterKey}|${sortOrder}`;
